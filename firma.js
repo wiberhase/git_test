45c291cf9a345b4a0f7d73cd590df5e096fdc675
wiberhase <218326739+wiberhase@users.noreply.github.com>
2026-01-10 22:40:32 -0600

    Agregacion de firma

---
+++ firma.js
@@ -0,0 +1,6 @@
+/* --- FIRMA DIGITAL WIBER.DEV --- */
+console.log(
+    "%c Desarrollado por Wiber %c \nSi te gusta el codigo, revisa mi GitHub: github.com/wiberhase ",
+    "background: #000; color: #00ff00; font-weight: bold; padding: 5px; border-radius: 4px; border: 1px solid #00ff00;", 
+    "color: #555; font-size: 11px;"
+);
